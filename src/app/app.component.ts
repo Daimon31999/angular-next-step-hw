2ed9c4f9fb332a02de225814893be5547d6360f4
Daimon31999 <dimahinev31999@icloud.com>
2021-06-06 14:42:56 +0300

    all done!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-currency-convertor-hw';
+
+  indianRupeeToMdl = 0.24;
+  myInput = new FormControl('');
+  mdl = 0;
+  ngOnInit() {
+    this.myInput.valueChanges.subscribe((value) => {
+      this.mdl = this.indianRupeeToMdl * Number(this.myInput.value);
+    });
+  }
 }
